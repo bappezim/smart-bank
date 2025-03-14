cb189ed06c306e00c235181d14c1805a8b719386
Haniel lima <haniellimasantos@gmail.com>
2025-03-14 13:36:34 -0300

    alterado 2

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+    <Image source={require('./logo.png')} />
+    <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text}
+        />-
+
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="useless placeholder"
+          keyboardType="numeric"
+        />
+
       <Text>ola novo aqui </Text>
       <StatusBar style="auto" />
     </View>
@@ -13,6 +28,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
